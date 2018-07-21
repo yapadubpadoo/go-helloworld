8d0ec515d7db571f5674540d31876613885f9d33
Hiruma <yapadubpadoo@gmail.com>
2018-07-21 15:18:02 +0700

    switch

--- main.go
+++ main.go
@@ -9,15 +9,15 @@ func init() {
 }
 
 func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
+	a := 10
+	b := 20
 
-	a := [...]int{1, 2, 3, 4, 5}
-	b := []int{1, 2, 3, 4, 5}
-
-	for i := 0; i < len(a); i++ {
-		fmt.Println(a[i])
-		b = append(b, a[i])
+	switch {
+	case a > 10:
+		fmt.Println("a > 10")
+	case b > 10:
+		fmt.Println("b > 10")
+	default:
+		fmt.Println("wtf")
 	}
 }
