6237747dd5ef17c7505271aed3c8e2012a0f4401
Hiruma <yapadubpadoo@gmail.com>
2018-07-21 15:23:18 +0700

    defer

--- main.go
+++ main.go
@@ -9,15 +9,7 @@ func init() {
 }
 
 func main() {
-	a := 10
-	b := 20
-
-	switch {
-	case a > 10:
-		fmt.Println("a > 10")
-	case b > 10:
-		fmt.Println("b > 10")
-	default:
-		fmt.Println("wtf")
-	}
+	defer fmt.Println("World")
+	defer fmt.Println("Middle")
+	fmt.Println("Hello")
 }
