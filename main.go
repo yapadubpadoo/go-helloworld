96a09c84b85ffb419a04ef3082e21b7dab7f04bd
Hiruma <yapadubpadoo@gmail.com>
2018-07-21 14:37:55 +0700

    update code

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yapadubpadoo/gotools"
+	"github.com/yapadubpadoo/gotools/stringhelper"
+)
+
+func init() {
+	fmt.Println("Init!")
+}
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println(gotools.Add(10, 2))
+	fmt.Println(stringhelper.Upper("cat"))
+}
